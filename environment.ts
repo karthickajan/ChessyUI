a230902fac149487bf991c65d9d1c874d575e4a9
karthickajan <karthickajangs@gmail.com>
2025-11-15 22:22:28 +0530

    feat: Configure production and development environments

--- environment.ts
+++ environment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
-    production: false,
-    HOST:'/api',
+    production: true,
+    // HOST:'/api',
     URL:'http://chessy-karthick-ajan-dev.apps.rm2.thpm.p1.openshiftapps.com',
   };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  URL: 'https://your-production-backend-url.com' // <-- IMPORTANT: Replace with your live backend URL
+  URL: 'http://chessy-karthick-ajan-dev.apps.rm2.thpm.p1.openshiftapps.com' // <-- IMPORTANT: Replace with your live backend URL
 };
